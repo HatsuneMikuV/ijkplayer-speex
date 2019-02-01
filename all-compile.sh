ea388e64f63dc3f8de567386752452f4dc3e56bc
Icarus <anglemiku.v@gmail.com>
2019-02-01 14:52:13 +0800

    [fix]  change sh more cpu arm

--- all-compile.sh
+++ all-compile.sh
@@ -2,6 +2,7 @@
 
 
 FF_ALL_ARCHS_CUSTOM_SDK="arm64"
+#xcode 9.0 & later only arm64
 #FF_ALL_ARCHS_CUSTOM_SDK="arm64 armv7s x86_64 i386 armv7"
 
 
@@ -10,11 +11,15 @@ FF_ALL_ARCHS=$FF_ALL_ARCHS_CUSTOM_SDK
 FF_ALL_SH="ogg speex speexdsp openssl ffmpeg"
 
 
-for FF_SH in $FF_ALL_SH
-do
-    sh init-ios-$FF_SH.sh $FF_ALL_ARCHS
-done
 
+
+for ARCH in $FF_ALL_ARCHS
+do
+    for FF_SH in $FF_ALL_SH
+    do
+    sh init-ios-$FF_SH.sh $ARCH
+    done
+done
 
 
 cd ios
@@ -24,6 +29,10 @@ do
     for ARCH in $FF_ALL_ARCHS
     do
     sh compile-$FF_SH.sh clean
+    done
+
+    for ARCH in $FF_ALL_ARCHS
+    do
     sh compile-$FF_SH.sh $ARCH
     done
 done
